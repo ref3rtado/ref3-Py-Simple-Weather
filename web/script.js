84fcd717245f9023be7c2a1cf273f40bd8d8df1a
ref3rtado <rfurtado3.dev@gmail.com>
2025-05-11 18:53:01 -0400

    Add basic HTML input field for zip code.

--- web/script.js
+++ web/script.js
@@ -3,6 +3,7 @@ console.log("Script loaded");
 //If grabbing a value from python function, it needs to be an async function
 //If you want to use the value in a variable, you need to use await
 
+//TODO: Grab input value from zipcode and pass to python
 async function poke_snake() {
     let response = await eel.poke_html()();
     console.log("Response from Python:", response);
