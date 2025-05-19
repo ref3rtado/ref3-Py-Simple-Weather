5089eae6dc004f35e94e0ac7cd35077483ba4c09
ref3rtado <rfurtado3.dev@gmail.com>
2025-05-19 19:48:09 -0400

    Filled out the parse_json function

--- SimpleWeather.py
+++ SimpleWeather.py
@@ -32,22 +32,53 @@ def get_zipcode(zipcode):
 #TODO: JavaScript should tell python what data it wants to display, pass it as an argument, which will return paired down map of relevant data
     #The JavaScript should translate the html select-options to the keys in the json object, and pass values(keys) to this funciton
 def parse_json(raw_json):
-    relevant_data = {}
-    relevant_data['location'] = [
+    location_info = {}
+    quick_look_weather = {}
+    detailed_weather = {}
+    location_info['location'] = [
         raw_json['location']['name'], 
         raw_json['location']['region'], 
         raw_json['location']['country']
         ]
-    relevant_data['temperature'] = [
-        raw_json['current']['temp_c'], 
-        raw_json['current']['temp_f']
+    location_info['local_time'] = [
+        raw_json['current']['localtime_epoch'],
+        raw_json['location']['tz_id']
         ]
-    relevant_data['overall_condition'] = [
+    quick_look_weather['temperature'] = [
+        raw_json['current']['temp_c'],
+        raw_json['current']['feelslike_c'], 
+        raw_json['current']['temp_f'],
+        raw_json['current']['feelslike_f']
+        ]
+    quick_look_weather['overall_condition'] = [
         raw_json['current']['condition']['text'], 
         raw_json['current']['condition']['icon']
         ]
-    #TODO: Make an eel-exposed JavaScript function to send this data to
-    eel.getRelevantData(relevant_data)
+    quick_look_weather['wind'] = [
+        raw_json['current']['wind_mph'],
+        raw_json['current']['wind_dir']
+        ]
+    detailed_weather['humidity'] = [
+        raw_json['current']['humidity'], 
+        ]
+    detailed_weather['precipitation'] = [
+        raw_json['current']['precip_mm'], 
+        raw_json['current']['precip_in']
+        ]
+    detailed_weather['pressure'] = [
+        raw_json['current']['pressure_mb'], 
+        raw_json['current']['pressure_in']
+        ]
+    detailed_weather['uv_index'] = [
+        raw_json['current']['uv'], 
+        ]
+    detailed_weather['visibility'] = [
+        raw_json['current']['vis_km'], 
+        raw_json['current']['vis_miles']
+        ]
+    #Put in the payload and send it to the front-end
+    payload = [location_info, quick_look_weather, detailed_weather] 
+    eel.getRelevantData(payload)
 
 # Initiate the webUI #
 if __name__ == "__main__":
